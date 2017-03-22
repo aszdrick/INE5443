a30dfdaaa6ad1d26931e84cf2e0f33f24e92384d
Ghabriel <ghabriel.nunes@hotmail.com>
2017-03-22 00:37:17 -0300

    Double spiral classifier

--- t1/classify.py
+++ t1/classify.py
@@ -11,10 +11,10 @@ import utils
 parser = argparse.ArgumentParser(description='Classify some data.')
 
 parser.add_argument('-k', type=int, default=1, help='k Nearest Neighbor classifier')
-parser.add_argument('-d', '--distance', choices=['euclidian', 'hamming+'], help='Distance metric algorithm')
+parser.add_argument('-d', '--distance', required=True, choices=['euclidian', 'hamming+'], help='Distance metric algorithm')
 parser.add_argument('-t', '--training_set', type=str, help='Filename of the training set, must be a CSV file')
 parser.add_argument('-i', '--input', type=str, help='Filename of the input to classify, must be a CSV file')
-parser.add_argument('-o', '--output', type=str, help='Output file')
+parser.add_argument('-o', '--output', required=True, type=str, help='Output file')
 parser.add_argument('-c', '--category', type=int, help='Column of the class')
 parser.add_argument('-I', '--ignore', action='append', type=int, help='Columns to be ignored')
 parser.add_argument('-s', '--spiral', type=str, choices=['single', 'double'], help='Number of spirals')
@@ -23,12 +23,12 @@ parser.add_argument('-n', '--noise', type=int, default=0, help='Noise for the sp
 parser.add_argument('-v', '--verbose', action='store_true', help='Print additional statistics')
 parser.add_argument('-p', '--plot', action='store_true', help='Plot the classified patterns (if 2D)')
 parser.add_argument('-l', '--slice', type=float, help='Automatically pick the given approximate percentage of the input set to use as the training set')
+parser.add_argument('-S', '--save_image', action='store_true', help='Save the resulting spiral as an image')
 
 args = parser.parse_args()
 
-if not args.distance or\
-   not args.output or\
-  (args.category == None) or\
+if not args.output or\
+  (not args.spiral and args.category == None) or\
   (not args.spiral and not args.input) or\
   (not args.spiral and args.noise) or\
   (args.spiral and (args.training_set or args.input)) or\
@@ -114,11 +114,35 @@ if not args.spiral:
         print("Fails: %i" % (fails))
         print("Precision: %f%%" % (100 * hits / (hits + fails)))
 else:
+    neighborhood = 2 * args.grid_size + 50
     if args.spiral == "single":
         data = single_spiral(args.grid_size, args.noise)
-        plt.scatter([tp[0] for tp in data], [tp[1] for tp in data], color="r")
+        # plt.scatter([tp[0] for tp in data], [tp[1] for tp in data], color="r")
     else:
-        data = double_spiral(args.grid_size, args.noise)
-        plt.scatter([tp[0] for tp in data[0]], [tp[1] for tp in data[0]], color="r")
-        plt.scatter([tp[0] for tp in data[1]], [tp[1] for tp in data[1]], color="b")
+        spirals = double_spiral(args.grid_size, args.noise)
+        spiral_points = [(s[0] + args.grid_size, s[1] + args.grid_size, 0) for s in spirals[0]]\
+                      + [(s[0] + args.grid_size, s[1] + args.grid_size, 1) for s in spirals[1]]
+        # spiral_points = spirals[0] + spirals[1]
+        red_points = []
+        blue_points = []
+        for x in range(neighborhood):
+            for y in range(neighborhood):
+                entry = (x, y)
+                class_value = cl.kNN(spiral_points, entry, distance_function, \
+                                     2, args.k)
+                if class_value == 0:
+                    red_points.append(entry)
+                else:
+                    blue_points.append(entry)
+
+        first = [(255, 0, 0) for point in red_points]
+        second = [(0, 0, 255) for point in blue_points]
+
+        if args.save_image:
+            import imagesaver
+            imagesaver.save(points, first + second, neighborhood, args.output)
+
+        # plt.scatter([tp[0] for tp in data[0]], [tp[1] for tp in data[0]], color="r")
+        # plt.scatter([tp[0] for tp in data[1]], [tp[1] for tp in data[1]], color="b")
     plt.show()
+
