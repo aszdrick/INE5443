d1f63f1b8b3b945884e0c166b175e6ec3dc6a83f
aszdrick <aszdrick@gmail.com>
2017-04-18 19:06:37 -0300

    improvements on argparser, still a work in progress

--- t2/classify.py
+++ t2/classify.py
@@ -4,43 +4,41 @@ import argparse
 #from core import main
 
 parser = argparse.ArgumentParser(description="Classify some data")
+
+mtlxcg = parser.add_mutually_exclusive_group()
+mtlxcg.add_argument("-p", "--plot", action="store_true", help="Plot the classified patterns (only works with 2D data)")
+mtlxcg.add_argument("-s", "--show", action="store_true", help="Plot the classified patterns (only works with 2D data)")
 parser.add_argument("-v", "--verbose", action="store_true", help="Print additional statistics")
 
-subparsers = parser.add_subparsers(help="Specify type of data to be classified")
+parser.add_argument("algorithm", choices=["IB1", "IB2"], help="Classification algorithm")
 
-dataset = subparsers.add_parser("dataset", help="Classify a given dataset")
+dataparsers = parser.add_subparsers(help="Specify type of data to be classified", dest="data_type")
+dataparsers.required = True
 
-dgroup0 = dataset.add_argument_group("splited input files (CSV only)")
-dgroup0.add_argument("-t", "--training_set", type=str, help="Filename of the training set")
-dg0exc0 = dgroup0.add_mutually_exclusive_group(required=True)
-dg0exc0.add_argument("-e", "--test_set", type=str, help="Filename of the test set")
-dg0exc0.add_argument("-d", "--data", type=str, help="Filename of the data to be classified")
+dataset = dataparsers.add_parser("dataset", help="Classify a given dataset")
+dataset.add_argument("-i", "--input", type=str, required=True, metavar="FILE", help="Filename of the dataset")
+dataset.add_argument("-c", "--category", type=int, required=True, metavar="INDEX", help="Column of the class")
 
-dgroup1 = dataset.add_argument_group("single input file (CSV only)")
-dgroup1.add_argument("-l", "--slice", type=float, help="Automatically pick the given approximate percentage of the input set to use as the training set")
-dgroup1.add_argument("-i", "--input", type=str, help="Filename of the input to classify")
+dtstmtx = dataset.add_mutually_exclusive_group(required=True)
+dtstmtx.add_argument("-e", "--test_set", type=str, metavar="FILE", help="Filename of the test set")
+dtstmtx.add_argument("-d", "--data", type=str, metavar="FILE", help="Filename of the data to be classified")
+dtstmtx.add_argument("-s", "--split", type=float, default=50, metavar="PERCENTAGE", help="Automatically pick the given approximate percentage of the input dataset to use as the test set")
 
-dataset.add_argument("-o", "--output", type=str, default="out.csv", help="Output file")
+dataset.add_argument("-o", "--output", type=str, default="out.csv", metavar="FILE", help="Save classified data as <FILE>.csv")
+dataset.add_argument("-I", "--ignore", nargs="+", type=int, metavar="INDEX", help="Columns to be ignored")
 
-image = subparsers.add_parser("image", help="Classify a given image")
+#image = dataparsers.add_parser("image", help="Classify a given image")
 
-spiral = subparsers.add_parser("spiral", help="Classify a given spiral")
-spiral.add_argument("-t", "--type", type=str, choices=["single", "double"], default="single", help="Number of spirals")
+spiral = dataparsers.add_parser("spiral", help="Classify a given spiral")
+spiral.add_argument("-t", "--type", type=str, choices=["single", "double"], default="single", help="Type of spiral")
 spiral.add_argument("-g", "--grid_size", type=int, default=50, help="Grid size to generate spiral (default 50)")
+spiral.add_argument("-n", "--noise", type=int, default=0, help="Noise for the spiral.")
+spiral.add_argument("-o", "--output", type=str, default="out.png", metavar="FILE", help="Save classified data as <FILE>.png")
 
-
-# spirals = parser.add_argument_group("Spirals")
-
-# parser.add_argument("-k", type=int, default=1, help="k Nearest Neighbor classifier")
-# parser.add_argument("-i", "--input", type=str, help="Filename of the input to classify, must be a CSV file")
-# parser.add_argument("-o", "--output", type=str, default="out", help="Output file")
-# parser.add_argument("-c", "--category", type=int, help="Column of the class")
-# parser.add_argument("-I", "--ignore", action="append", type=int, help="Columns to be ignored")
-# parser.add_argument("-n", "--noise", type=int, default=0, help="Noise for the spiral(s).")
-# parser.add_argument("-p", "--plot", action="store_true", help="Plot the classified patterns (if 2D)")
-# parser.add_argument("-S", "--save_image", action="store_true", help="Save the resulting spiral as an image")
 # parser.add_argument("-C", "--compare", nargs="+", choices=["l2norm", "quadratic_mahalanobis", "linear_mahalanobis"], help="Compare with other algorithms")
 
-args = parser.parse_args()
-print(args)
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print(args)
+
 # main(parser, args)
