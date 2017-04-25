624bfaa9562a4bbee3dfbcf3cb53ce6b13ac9660
Ghabriel Nunes <ghabriel.nunes@gmail.com>
2017-04-25 09:00:56 -0300

    Created a new script to set up the virtual environment

--- t2/virtual_env/env_setup.sh
+++ t2/virtual_env/env_setup.sh
@@ -2,10 +2,13 @@
 
 wget https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz#md5=44e19f4134906fe2d75124427dc9b716
 tar -xf virtualenv-15.1.0.tar.gz
+rm virtualenv-15.1.0.tar.gz
 cd virtualenv-15.1.0
 python3 virtualenv.py ../sandbox
 
-echo 
-echo '--------------------------------'
-echo 'Execute ". sandbox/bin/activate"'
-echo
+if [ "$#" -lt 1 ]; then
+	echo 
+	echo '--------------------------------'
+	echo 'Execute ". sandbox/bin/activate"'
+	echo
+fi
---
+++ t2/virtual_env/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Usage: . ./setup.sh
+./env_setup.sh 0
+. sandbox/bin/activate
+../install_dependencies.sh
