65edbf18c78d1dce2bb9a9e6aae350b2cc006b7d
aszdrick <aszdrick@gmail.com>
2017-04-26 23:15:31 -0300

    new version of arguments

--- t2/arguments.py
+++ t2/arguments.py
@@ -1,21 +1,12 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="Classify some data")
+common = argparse.ArgumentParser(add_help=False)
+common.add_argument("-s", "--show", action="store_true", help="Show the classified patterns (only works with 2D data)")
 
-parser.add_argument("-k", "--knn", type=int, default=1, metavar="NUMBER", help="Use the k nearest neighbors to classify data")
-parser.add_argument("-s", "--show", action="store_true", help="Show the classified patterns (only works with 2D data)")
-
-parser.add_argument("-zpa", "--z_precision_acceptance", type=float, default=0.9, help="z-Value for Acceptance Precision Interval")
-parser.add_argument("-zpd", "--z_precision_dropping", type=float, default=0.75, help="z-Value for Dropping Precision Interval")
-parser.add_argument("-zfa", "--z_frequency_acceptance", type=float, default=0.9, help="z-Value for Acceptance Frquency Interval")
-parser.add_argument("-zfd", "--z_frequency_dropping", type=float, default=0.75, help="z-Value for Dropping Frquency Interval")
-
-parser.add_argument("algorithm", choices=["IB1", "IB2", "IB3", "IB4"], help="Classification algorithm")
-
-dataparsers = parser.add_subparsers(help="Specify type of data to be classified", dest="data_type")
+dataparsers = common.add_subparsers(help="Type of data to be classified", dest="data_type")
 dataparsers.required = True
 
-dataset = dataparsers.add_parser("dataset", help="Classify a given dataset")
+dataset = dataparsers.add_parser("dataset")
 dataset.add_argument("-i", "--input", type=str, required=True, metavar="FILE", help="Filename of the dataset")
 dataset.add_argument("-c", "--category", type=int, required=True, metavar="INDEX", help="Column of the class")
 dataset.add_argument("-d", "--data", type=str, metavar="FILE", help="Filename of the data to be classified")
@@ -27,8 +18,27 @@ dtstmtx.add_argument("-s", "--split", type=float, metavar="PERCENTAGE", help="Au
 dataset.add_argument("-o", "--output", type=str, default="out", metavar="FILE", help="Save classified data as <FILE>.csv")
 dataset.add_argument("-I", "--ignore", nargs="+", type=int, metavar="INDEX", help="Columns to be ignored")
 
-spiral = dataparsers.add_parser("spiral", help="Classify a given spiral")
+spiral = dataparsers.add_parser("spiral")
 spiral.add_argument("-t", "--spiral_type", type=str, choices=["single", "double"], default="single", help="Type of spiral")
 spiral.add_argument("-g", "--grid_size", type=int, default=50, help="Grid size to generate spiral (default 50)")
 spiral.add_argument("-n", "--noise", type=int, default=0, help="Noise for the spiral.")
 spiral.add_argument("-o", "--output", type=str, default="out", metavar="FILE", help="Save classified data as <FILE>.png")
+
+zargs = argparse.ArgumentParser(add_help=False)
+zargs.add_argument("-zpa", "--z_precision_acceptance", type=float, default=0.9, metavar="FLOAT", help="z-Value for Acceptance Precision Interval")
+zargs.add_argument("-zpd", "--z_precision_dropping", type=float, default=0.75, metavar="FLOAT", help="z-Value for Dropping Precision Interval")
+zargs.add_argument("-zfa", "--z_frequency_acceptance", type=float, default=0.9, metavar="FLOAT", help="z-Value for Acceptance Frquency Interval")
+zargs.add_argument("-zfd", "--z_frequency_dropping", type=float, default=0.75, metavar="FLOAT", help="z-Value for Dropping Frquency Interval")
+
+kdargs = argparse.ArgumentParser(add_help=False)
+kdargs.add_argument("-k", "--knn", type=int, default=1, metavar="NUMBER", help="Use the k nearest neighbors to classify data")
+
+parser = argparse.ArgumentParser(description="Instance-based Learning classificator")
+
+algparsers = parser.add_subparsers(help="Classification algorithm", dest="algorithm")
+algparsers.required = True
+
+ib1 = algparsers.add_parser("IB1", parents=[common, kdargs])
+ib2 = algparsers.add_parser("IB2", parents=[common, kdargs])
+ib3 = algparsers.add_parser("IB3", parents=[common, kdargs, zargs])
+ib4 = algparsers.add_parser("IB4", parents=[common, zargs])
