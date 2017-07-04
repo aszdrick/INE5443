8f91afd9f16637c14417023711209685860707b1
aszdrick <aszdrick@gmail.com>
2017-07-04 16:51:18 -0300

    better structure of recursive functions to traverse the tree

--- t5/dendrogram.py
+++ t5/dendrogram.py
@@ -8,67 +8,38 @@ xoffset = 0.5
 color = "k"
 xgrid = False
 
+# class to encapsulate the fitness function, i.e.,
+# how good is a level using the specified set of weights and
+# interval.
 class Classifier:
     # weights = (size weight, average weight, stddev weight)
-    # priority = desirable value of size
-    def __init__(self, weights, priority, interval):
+    def __init__(self, weights, interval):
         self.weights = weights
-        self.priority = priority
         self.interval = interval
 
     def __call__(self, values):
         vs = []
-        vs.append(1 / (abs(self.priority - values[0]) + 1))
-        vs.append(self.min_average / values[1])
-        vs.append(self.min_stddev / values[2])
+        vs.append(self.min_average / values[0])
+        vs.append(self.min_stddev / values[1])
         return sum([w * v for w, v in zip(self.weights, vs)])
 
 def __visit_cluster(tree, labels, xticks, counter = 0):
-    if isinstance(tree[0], tuple):
-        (blx, bly) = __visit_cluster(tree[0], labels, xticks)
+    if isinstance(tree, tuple):
+        bottom_left = __visit_cluster(tree[0], labels, xticks)
+        bottom_right = __visit_cluster(tree[1], labels, xticks)
     else:
-        (blx, bly) = (0, len(labels))
-        labels.append(tree[0])
-    if isinstance(tree[1], tuple):
-        (brx, bry) = __visit_cluster(tree[1], labels, xticks)
-    else:
-        (brx, bry) = (0, len(labels))
-        labels.append(tree[1])
-
-    (ulx, uly) = (tree[2], bly)
-    (urx, ury) = (tree[2], bry)
+        labels.append(tree)
+        return (0, len(labels) - 1)
 
     plt.plot(
-        [blx, ulx, urx, brx],
-        [bly, uly, ury, bry],
+        [bottom_left[0], tree[2], tree[2], bottom_right[0]],
+        [bottom_left[1], bottom_left[1], bottom_right[1], bottom_right[1]],
         color,
         linewidth = linewidth)
 
     xticks.append(tree[2])
 
-    return (tree[2], (bly + bry) / 2)
-
-
-# def plot_twist(tree):
-#     labels = []
-#     xticks = [0]
-#     label_xs = {}
-
-#     for node in tree:
-#         if len(node[0]) == 1:
-#             label_xs[node[0][0]] = len(labels)
-#             labels.append(node[0][0])
-#             (blx, bly) = (0, len(labels))
-#         else:
-#             values = [label_xs[l] for l in node[0]]
-#             blx = (max(values) + min(values)) / 2
-#         if len(node[1]) == 1:
-#             label_xs[node[1][0]] = len(labels)
-#             labels.append(node[1][0])
-#             (brx, bry) = (0, len(labels))
-#         else:
-#             values = [label_xs[l] for l in node[1]]
-#             middle = (max(values) + min(values)) / 2
+    return (tree[2], (bottom_left[1] + bottom_right[1]) / 2)
 
 def __plot_tree(tree):
     labels = []
@@ -90,7 +61,6 @@ def __plot_subtrees(trees):
     return (labels, xticks)
 
 
-# tree = (((A, B, dist), C, dist), )
 def plot(data):
     if isinstance(data, list):
         (labels, xticks) = __plot_subtrees(data)
@@ -111,32 +81,28 @@ def plot(data):
 
 def labels_of(tree):
     labels = []
-    if isinstance(tree[0], tuple):
+    if isinstance(tree, tuple):
         labels += labels_of(tree[0])
-    else:
-        labels.append(tree[0])
-    if isinstance(tree[1], tuple):
         labels += labels_of(tree[1])
     else:
-        labels.append(tree[1])
+        labels.append(tree)
     return labels
 
 def __levelize(tree, levels, counter = 0):
-    if isinstance(tree[0], tuple):
+    if isinstance(tree, tuple):
         __levelize(tree[0], levels, counter + 1)
-    if isinstance(tree[1], tuple):
         __levelize(tree[1], levels, counter + 1)
     if counter not in levels.keys():
         levels[counter] = []
     levels[counter].append(tree)
 
-def levels_of(tree):
+def __levels_of(tree):
     levels = {}
     __levelize(tree, levels)
     return levels
 
-def best_level(levels, weights, interval, priority):
-    score_it = Classifier(weights, priority, interval)
+def __best_level(levels, weights, interval):
+    score_it = Classifier(weights, interval)
     statistics = {}
     min_average = maxsize
     min_stddev = maxsize
@@ -146,7 +112,7 @@ def best_level(levels, weights, interval, priority):
         groups = len(distances)
         xm = sum(distances) / groups
         stddev = math.sqrt(sum(map(lambda x: (x - xm) ** 2, distances)))
-        statistics[i] = (groups, xm, stddev)
+        statistics[i] = (xm, stddev)
         if xm < min_average:
             min_average = xm
         if stddev < min_stddev:
@@ -168,23 +134,20 @@ def best_level(levels, weights, interval, priority):
     print("best score is", best_score)
     return best_level
 
-def cut(tree, weights, interval, priority = None):
+def cut(tree, weights, interval):
     labels = labels_of(tree)
-    levels = levels_of(tree)
+    levels = __levels_of(tree)
     max_level = max(levels.keys())
-    if not priority:
-        priority = round((interval[0] + interval[1]) / 2)
 
     for i in range(max_level + 1):
         level_labels = []
         for tup in levels[i]:
             level_labels += labels_of(tup)
         length = len(levels[i])
-        # len(level_labels) != len(labels)
         if length > interval[1] or length < interval[0]:
             del levels[i]
 
-    chosen_level = best_level(levels, weights, interval, priority)
+    chosen_level = __best_level(levels, weights, interval)
 
     trees = [tree]
     for i in range(chosen_level):
